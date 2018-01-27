6ff749cf2bf53c556fdd7e90cf7027970b1205b4
Ville Ronkainen <finvpower@gmail.com>
2018-01-27 21:07:27 +0200

    Spreading fire

---
+++ FireDrill/Assets/Scripts/Fire.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+    enum FireDirection
+    {
+        Left, Right, Up, Down, None
+    }
+
+    float spreadTime = 0.5f;
+    float spreadTimer = 0.0f;
+    public Transform flame;
+    List<Transform> flames;
+    public Vector3 startPos;
+    BoxCollider2D selfCollider;
+
+	// Use this for initialization
+	void Start () {
+        flames = new List<Transform>();
+        Transform obj = Instantiate(flame);
+        obj.transform.position = startPos;
+        flames.Add(obj);
+        selfCollider = GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        spreadTimer += Time.fixedDeltaTime;
+        if (spreadTimer >= spreadTime)
+        {
+            spreadTimer = 0.0f;
+            SpreadFire();
+        }
+    }
+
+    private void SpreadFire()
+    {
+        int flamesCount = flames.Count;
+        int index = 0;
+        FireDirection freeDir = FireDirection.None;
+        bool looping = true;
+        while (looping)
+        {
+            Debug.Log("looping");
+            index = Random.Range(0, flamesCount);
+            freeDir = GetFreeDirection(flames[index]);
+            if (freeDir != FireDirection.None)
+            {
+                looping = false;
+            }
+        }
+
+        Transform obj = Instantiate(flame);
+        obj.transform.position = flames[index].transform.position + GetSpreadOffset(freeDir);
+        flames.Add(obj);
+    }
+
+    private FireDirection GetFreeDirection(Transform obj)
+    {
+        int startDir = Random.Range(0, (int)FireDirection.None);
+        int currentDir = (startDir + 1) % (int)FireDirection.None;
+        bool looped = false;
+        while (!looped)
+        {
+            Debug.Log("not looped yet");
+            Vector2 rayDir = RayDirection((FireDirection)currentDir);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(obj.transform.position + new Vector3(rayDir.x, rayDir.y) * 0.32f, RayDirection((FireDirection)currentDir), 0.05f);
+            Debug.DrawRay(obj.transform.position + new Vector3(rayDir.x, rayDir.y) * 0.32f, RayDirection((FireDirection)currentDir) * 0.05f);
+            bool flameHit = false;
+            foreach (RaycastHit2D hit in hits)
+            {
+                Debug.Log(hit.collider.tag + " " + hit.collider.name);
+                Debug.Log(hit.collider != selfCollider);
+                if (hit.collider != selfCollider &&
+                    hit.collider.tag == "Fire")
+                {
+                    flameHit = true;
+                    Debug.Log("cant spawn");
+                    break;
+                }
+            }
+
+            if (!flameHit)
+            {
+                Debug.Log("direction selected");
+                return (FireDirection)currentDir;
+            }
+
+            if (currentDir == startDir)
+            {
+                looped = true;
+            }
+
+            currentDir = (currentDir + 1) % (int)FireDirection.None;
+        }
+
+        return FireDirection.None;
+    }
+
+    private Vector2 RayDirection(FireDirection dir)
+    {
+        switch (dir)
+        {
+            case FireDirection.Up:
+                return Vector2.down;
+                break;
+
+            case FireDirection.Down:
+                return Vector2.up;
+                break;
+
+            case FireDirection.Left:
+                return Vector2.left;
+                break;
+
+            case FireDirection.Right:
+                return Vector2.right;
+                break;
+
+            default:
+                return new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    private Vector3 GetSpreadOffset(FireDirection dir)
+    {
+        switch (dir)
+        {
+            case FireDirection.Up:
+                return new Vector3(0.0f, -0.32f, 0.0f);
+                break;
+
+            case FireDirection.Down:
+                return new Vector3(0.0f, 0.32f, 0.0f);
+                break;
+
+            case FireDirection.Left:
+                return new Vector3(-0.32f, 0.0f, 0.0f);
+                break;
+
+            case FireDirection.Right:
+                return new Vector3(0.32f, 0.0f, 0.0f);
+                break;
+
+            default:
+                return new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+
+}
